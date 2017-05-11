e6c1ee48bebefeae57afc36b85a23b30e4a510fe
Марина Звягина <zvyagina.marina@gmail.com>
2017-05-11 15:50:35 +0300

    Initial Commit

---
+++ MapsAppWithTabs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapsAppWithTabs
+//
+//  Created by Марина Звягина on 11.05.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapsAppWithTabs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapsAppWithTabs
+//
+//  Created by Марина Звягина on 11.05.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
