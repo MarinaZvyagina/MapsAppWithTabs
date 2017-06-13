2f2e6f852aa233a67febff9f5cbe8ab008447b48
Марина Звягина <zvyagina.marina@gmail.com>
2017-06-13 11:10:49 +0300

    Main tab bar controller that create map and list tabs and set icons for them

--- MapsAppWithTabs/ViewController.m
+++ MapsAppWithTabs/ViewController.m
@@ -7,22 +7,46 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import "MAWTMapViewController.h"
+#import "MAWTTableTableViewController.h"
+#import "MAWGooglePlacesData.h"
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    MAWTMapViewController *controller1 = [[MAWTMapViewController alloc] initWithDataManager:[MAWGooglePlacesData new]];
+    MAWTTableTableViewController *controller2 = [[MAWTTableTableViewController alloc] initWithMapViewManager:controller1];
+    
+    self.viewControllers = [NSArray arrayWithObjects:
+                                        controller1,
+                                        controller2,
+                                        nil];
+    NSString *imageFilePath = [[NSBundle mainBundle] pathForResource:@"map" ofType:@"png"];
+    UITabBarItem * item = controller1.tabBarItem;
+    [self setImageWithPath:imageFilePath forItem:item];
+    imageFilePath = [[NSBundle mainBundle] pathForResource:@"list" ofType:@"png"];
+    item = controller2.tabBarItem;
+    [self setImageWithPath:imageFilePath forItem:item];
 }
 
+-(void)setImageWithPath:(NSString *)imageFilePath forItem:(UITabBarItem *)item {
+    UIImage *imageObject = [UIImage imageWithContentsOfFile:imageFilePath];
+    UIImage * compressedImage = [self imageWithImage:imageObject scaledToSize:CGSizeMake(30, 30)];
+    [item setImage:compressedImage];
+}
+
+-(UIImage *)imageWithImage:(UIImage *)imageToCompress scaledToSize:(CGSize)newSize {
+    
+    UIGraphicsBeginImageContextWithOptions(newSize, NO, 0.0);
+    [imageToCompress drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
